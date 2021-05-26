7769dee5e63919d40c1f21915e91d2b2f0c8c87f
Sarvabowmen <sarvabowmen@gmail.com>
2021-05-26 20:10:07 +0530

    updated steps for subscribing to notifications

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,15 @@ import SubmitUserInfo from './SubmitUserInfo';
 const Home = () => (
   <div className="container">
     <SubmitUserInfo></SubmitUserInfo>
-    <div style={{ padding: "40px", color: "green" }}><strong>To get instant COVID-19 Vaccination availablity notifications please subscribe to <a href="https://t.me/cowinvaccinenotifications">https://t.me/cowinvaccinenotifications</a></strong></div>
+    <div style={{ padding: "40px", color: "green" }}>
+      <div style={{ padding: "20px 0px", color: "black" }}>
+       <div> Steps to subscribe for notifications:</div>
+       <div>1. Open Telegram and go to search bar</div>
+       <div>2. Paste the link given above</div>
+       <div>3. Click on JOIN</div>
+      </div>
+      <strong>To get instant COVID-19 Vaccination availablity notifications please subscribe to <a href="https://t.me/cowinvaccinenotifications">https://t.me/cowinvaccinenotifications</a></strong>
+    </div>
   </div>
 );
 
