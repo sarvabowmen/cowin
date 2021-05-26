6d5f0e773ee90dbb68901bb0f93ed78fc5c7a411
Sarvabowmen <sarvabowmen@gmail.com>
2021-05-26 19:36:17 +0530

    updated telegram link

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,6 +5,7 @@ import SubmitUserInfo from './SubmitUserInfo';
 const Home = () => (
   <div className="container">
     <SubmitUserInfo></SubmitUserInfo>
+    <div style={{ padding: "40px", color: "green" }}><strong>To get instant COVID-19 Vaccination availablity notifications please subscribe to <a href="https://t.me/cowinvaccinenotifications">https://t.me/cowinvaccinenotifications</a></strong></div>
   </div>
 );
 
